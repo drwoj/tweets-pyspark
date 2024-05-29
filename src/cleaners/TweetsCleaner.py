d88028bf9a2383aa4361d7fbb6d999ad769bbc50
Wojciech Dranka <dr.woj@wp.pl>
2024-05-29 21:08:23 +0200

    Unify hashtags to lowercase

--- src/cleaners/TweetsCleaner.py
+++ src/cleaners/TweetsCleaner.py
@@ -1,13 +1,14 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import regexp_replace, split, col
+from pyspark.sql.functions import regexp_replace, split, col, lower
 from src.utils.columns import Columns
 
 
 class TweetsCleaner:
     @classmethod
     def clean_tweets(cls, tweets: DataFrame) -> DataFrame:
-        tweets = (tweets.withColumn(Columns.HASHTAGS.value, regexp_replace(col(Columns.HASHTAGS.value), "^\\[|\\]$", ''))
-                  .withColumn(Columns.HASHTAGS.value, regexp_replace(col(Columns.HASHTAGS.value), "'", ''))
-                  .withColumn(Columns.HASHTAGS.value, split(col(Columns.HASHTAGS.value), ', ')))
+        tweets = tweets.withColumn(Columns.HASHTAGS.value, regexp_replace(col(Columns.HASHTAGS.value), "^\\[|\\]$", '')) \
+                  .withColumn(Columns.HASHTAGS.value, regexp_replace(col(Columns.HASHTAGS.value), "'", '')) \
+                  .withColumn(Columns.HASHTAGS.value, lower(col(Columns.HASHTAGS.value))) \
+                  .withColumn(Columns.HASHTAGS.value, split(col(Columns.HASHTAGS.value), ', '))
 
         return tweets
